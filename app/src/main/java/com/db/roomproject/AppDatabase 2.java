e0f9b0e6a0f5fcbe7e60e37d0d7ba6f3de4f4c91
balllm <148713797+balllm@users.noreply.github.com>
2024-07-13 20:10:25 +0500

    ь
    ь

--- app/src/main/java/com/db/roomproject/AppDatabase 2.java
+++ app/src/main/java/com/db/roomproject/AppDatabase 2.java
@@ -6,10 +6,11 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {User.class}, version = 1, exportSchema =
-        false)
+@Database(entities = {User.class, Item.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
+    public abstract ItemDao itemDao();
+
     private static volatile AppDatabase INSTANCE;
     public static AppDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
